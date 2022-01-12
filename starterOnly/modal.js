b7e31ed6e364f4e15a38bbe65f44199b718f5139
Loïs <flojuve@hotmail.fr>
2022-01-12 16:37:12 +0100

    issue #1

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,20 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const btnClose = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// close modal event
+btnClose.addEventListener("click", close);
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
 
+function close() {
+  modalbg.style.display = "none";
+}
 
